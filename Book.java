9309f204bc97f5308c8ca3ae4add72084027e025
chrysaliszt <cdoan925@students.fairmontschools.com>
2023-02-05 19:31:30 -0800

    added Book class attributes, constructor, & mutator methods

--- Book.java
+++ Book.java
@@ -1,3 +1,35 @@
 public class Book {
     
+    // ATTRIBUTES
+    String title;
+    String subject;
+    int yearOfPublication;
+    int numPages;
+    float reviewRating;
+
+    // CONSTRUCTOR
+    public Book() {
+        // empty constructor. will specify attributes in mutator methods.
+    }
+
+    // MUTATOR METHODS
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    public void setSubject(String newSubject) {
+        this.subject = newSubject;
+    }
+
+    public void setYearOfPublication(int newYearOfPublication) {
+        this.yearOfPublication = newYearOfPublication;
+    }
+
+    public void setNumPages(int newNumPages) {
+        this.numPages = newNumPages;
+    }
+
+    public void set(float newReviewRating) {
+        this.reviewRating = newReviewRating;
+    }
 }
